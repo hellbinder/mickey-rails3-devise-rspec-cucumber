0127ca1ffa21ac0a005068516e8b541f59b4d57f
name <email>
2013-03-27 00:03:02 -0400

    new views and migratiosn to work with devise

---
+++ db/migrate/20130327030301_add_confirmable_to_user.rb
@@ -0,0 +1,16 @@
+class AddConfirmableToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :confirmation_token, :string
+    add_column :users, :confirmed_at, :datetime
+    add_column :users, :confirmation_sent_at, :datetime
+    add_column :users, :unconfirmed_email, :string
+    add_index  :users, :confirmation_token, :unique => true
+  end
+  def self.down
+    remove_index  :users, :confirmation_token
+    remove_column :users, :unconfirmed_email
+    remove_column :users, :confirmation_sent_at
+    remove_column :users, :confirmed_at
+    remove_column :users, :confirmation_token
+  end
+end
